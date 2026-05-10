486dd0e080a442747a498b61759c3a77fe061875
immortals1278 <3156160490@qq.com>
2026-05-10 21:34:48 +0800

    成功播放动画

--- src/main/java/com/example/examplemod/TestMonsterEntity.java
+++ src/main/java/com/example/examplemod/TestMonsterEntity.java
@@ -9,10 +9,16 @@ import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.animation.RawAnimation;
+import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class TestMonsterEntity extends PathfinderMob implements GeoEntity {
+    private static final RawAnimation WALK_ANIM = RawAnimation.begin().thenLoop("walk");
+    private static final RawAnimation STAND_ANIM = RawAnimation.begin().thenLoop("stand");
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     protected TestMonsterEntity(EntityType<? extends PathfinderMob> pEntityType, Level pLevel) {
@@ -21,11 +27,20 @@ public class TestMonsterEntity extends PathfinderMob implements GeoEntity {
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        controllers.add(new AnimationController<>(this, "movement_controller", 5, this::movementPredicate));
     }
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.cache;
+    }
+
+    private PlayState movementPredicate(AnimationState<TestMonsterEntity> animationState) {
+        if (animationState.isMoving()) {
+            return animationState.setAndContinue(WALK_ANIM);
+        }
+
+        return animationState.setAndContinue(STAND_ANIM);
     }
 
     @Override
